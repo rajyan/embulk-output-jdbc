0a5053e3cc4180f1fd4abdd46108c2a125de371d
Yohta Kimura <38206553+rajyan@users.noreply.github.com>
2024-02-10 14:58:34 +0900

    test

---
+++ embulk-output-redshift/src/test/java/org/embulk/output/postgresql/BasicTest.java
@@ -0,0 +1,158 @@
+package org.embulk.output.postgresql;
+
+import static org.embulk.output.postgresql.PostgreSQLTests.execute;
+import static org.embulk.output.postgresql.PostgreSQLTests.selectRecords;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import org.embulk.config.ConfigDiff;
+import org.embulk.config.ConfigSource;
+import org.embulk.formatter.csv.CsvFormatterPlugin;
+import org.embulk.input.file.LocalFileInputPlugin;
+import org.embulk.output.PostgreSQLOutputPlugin;
+import org.embulk.output.file.LocalFileOutputPlugin;
+import org.embulk.parser.csv.CsvParserPlugin;
+import org.embulk.spi.FileInputPlugin;
+import org.embulk.spi.FileOutputPlugin;
+import org.embulk.spi.FormatterPlugin;
+import org.embulk.spi.OutputPlugin;
+import org.embulk.spi.ParserPlugin;
+import org.embulk.test.EmbulkTests;
+import org.embulk.test.TestingEmbulk;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class BasicTest
+{
+    private static final String BASIC_RESOURCE_PATH = "/org/embulk/output/postgresql/test/expect/basic/";
+
+    private static ConfigSource loadYamlResource(TestingEmbulk embulk, String fileName)
+    {
+        return embulk.loadYamlResource(BASIC_RESOURCE_PATH + fileName);
+    }
+
+    private static String readResource(String fileName)
+    {
+        String ret = EmbulkTests.readResource(BASIC_RESOURCE_PATH + fileName);
+        return ret;
+    }
+
+    @Rule
+    public TestingEmbulk embulk = TestingEmbulk.builder()
+            .registerPlugin(FileInputPlugin.class, "file", LocalFileInputPlugin.class)
+            .registerPlugin(ParserPlugin.class, "csv", CsvParserPlugin.class)
+            .registerPlugin(FormatterPlugin.class, "csv", CsvFormatterPlugin.class)
+            .registerPlugin(FileOutputPlugin.class, "file", LocalFileOutputPlugin.class)
+            .registerPlugin(OutputPlugin.class, "postgresql", PostgreSQLOutputPlugin.class)
+            .build();
+
+    private ConfigSource baseConfig;
+
+    @Before
+    public void setup() throws IOException
+    {
+        baseConfig = PostgreSQLTests.baseConfig();
+        execute(readResource("setup.sql")); // setup rows
+
+        if (System.getProperty("path.separator").equals(";")) {
+            // for Windows (because encoding will be set to UTF8 when executed by Eclipse)
+            System.setProperty("file.encoding", "MS932");
+        }
+    }
+
+    @Test
+    public void testNumber() throws Exception
+    {
+        Path in1 = toPath("test_number.csv");
+        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_number.yml")), in1);
+        assertThat(selectRecords(embulk, "test_number"), is(readResource("test_number_expected.csv")));
+        //assertThat(result1.getConfigDiff(), is((ConfigDiff) loadYamlResource(embulk, "test_expected.diff")));
+    }
+
+    @Test
+    public void testString() throws Exception
+    {
+        Path in1 = toPath("test_string.csv");
+        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_string.yml")), in1);
+        assertThat(selectRecords(embulk, "test_string"), is(readResource("test_string_expected.csv")));
+        //assertThat(result1.getConfigDiff(), is((ConfigDiff) loadYamlResource(embulk, "test_expected.diff")));
+    }
+
+    @Test
+    public void testTimestamp() throws Exception
+    {
+        Path in1 = toPath("test_timestamp.csv");
+        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_timestamp.yml")), in1);
+        assertThat(selectRecords(embulk, "test_timestamp"), is(readResource("test_timestamp_expected.csv")));
+        //assertThat(result1.getConfigDiff(), is((ConfigDiff) loadYamlResource(embulk, "test_expected.diff")));
+    }
+
+    @Test
+    public void testJson() throws Exception
+    {
+        Path in1 = toPath("test_json.csv");
+        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_json.yml")), in1);
+        assertThat(selectRecords(embulk, "test_json"), is(readResource("test_json_expected.csv")));
+        //assertThat(result1.getConfigDiff(), is((ConfigDiff) loadYamlResource(embulk, "test_expected.diff")));
+    }
+
+    @Test
+    public void testMerge() throws Exception
+    {
+        Path in1 = toPath("test_merge.csv");
+        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_merge.yml")), in1);
+        assertThat(selectRecords(embulk, "test_merge"), is(readResource("test_merge_expected.csv")));
+    }
+
+    @Test
+    public void testMergeWithKeys() throws Exception
+    {
+        Path in1 = toPath("test_merge_keys.csv");
+        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_merge_keys.yml")), in1);
+        assertThat(selectRecords(embulk, "test_merge"), is(readResource("test_merge_keys_expected.csv")));
+    }
+
+    @Test
+    public void testMergeRule() throws Exception
+    {
+        Path in1 = toPath("test_merge.csv");
+        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_merge_rule.yml")), in1);
+        assertThat(selectRecords(embulk, "test_merge"), is(readResource("test_merge_rule_expected.csv")));
+    }
+
+    @Test
+    public void testReplace() throws Exception
+    {
+        Path in1 = toPath("test_string.csv");
+        TestingEmbulk.RunResult result1 = embulk.runOutput(baseConfig.merge(loadYamlResource(embulk, "test_replace.yml")), in1);
+        assertThat(selectRecords(embulk, "test_string"), is(readResource("test_replace_expected.csv")));
+        //assertThat(result1.getConfigDiff(), is((ConfigDiff) loadYamlResource(embulk, "test_expected.diff")));
+    }
+
+
+    private Path toPath(String fileName) throws URISyntaxException
+    {
+        URL url = EmbulkTests.class.getResource(BASIC_RESOURCE_PATH + fileName);
+        return FileSystems.getDefault().getPath(new File(url.toURI()).getAbsolutePath());
+    }
+
+    @Test
+    public void testRoleName() throws Exception
+    {
+        Path in1 = toPath("test_string.csv");
+        ConfigSource config = baseConfig.merge(loadYamlResource(embulk, "test_string.yml"));
+        config.set("role_name", baseConfig.get(String.class, "user"));
+        TestingEmbulk.RunResult result1 = embulk.runOutput(config, in1);
+        assertThat(selectRecords(embulk, "test_string"), is(readResource("test_string_expected.csv")));
+        //assertThat(result1.getConfigDiff(), is((ConfigDiff) loadYamlResource(embulk, "test_expected.diff")));
+    }
+
+}
---
+++ embulk-output-redshift/src/test/java/org/embulk/output/postgresql/PostgreSQLTests.java
@@ -0,0 +1,76 @@
+package org.embulk.output.postgresql;
+
+import org.embulk.test.EmbulkTests;
+import org.embulk.test.TestingEmbulk;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.embulk.config.ConfigSource;
+
+import static java.util.Locale.ENGLISH;
+import static java.util.stream.Collectors.joining;
+import static org.embulk.test.EmbulkTests.readSortedFile;
+
+public class PostgreSQLTests
+{
+    public static ConfigSource baseConfig()
+    {
+        return EmbulkTests.config("EMBULK_OUTPUT_REDSHIFT_TEST_CONFIG");
+    }
+
+    public static void execute(String sql)
+    {
+        System.out.println(sql);
+        ConfigSource config = baseConfig();
+        ProcessBuilder pb = new ProcessBuilder(
+                "psql", "-w",
+                "--set", "ON_ERROR_STOP=1",
+                "--host", config.get(String.class, "host"),
+                "--username", config.get(String.class, "user"),
+                "--dbname", config.get(String.class, "database"),
+                "-c", sql);
+        pb.environment().put("PGPASSWORD", config.get(String.class, "password"));
+        pb.redirectErrorStream(true);
+        int code;
+        try {
+            Process process = pb.start();
+            InputStream inputStream = process.getInputStream();
+            byte[] buffer = new byte[8192];
+            int readSize;
+            while ((readSize = inputStream.read(buffer)) != -1) {
+                System.out.write(buffer, 0, readSize);
+            }
+            code = process.waitFor();
+        } catch (IOException | InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+        if (code != 0) {
+            throw new RuntimeException(String.format(ENGLISH,
+                        "Command finished with non-zero exit code. Exit code is %d.", code));
+        }
+    }
+
+    public static String selectRecords(TestingEmbulk embulk, String tableName) throws IOException
+    {
+        Path temp = embulk.createTempFile("txt");
+        Files.delete(temp);
+        execute("\\copy " + tableName + " to '" + temp.toString().replace("\\", "\\\\") + "' delimiter ','");
+        return readSortedFile(temp);
+    }
+
+    public static String selectRecords(TestingEmbulk embulk, String tableName, List<String> columnList) throws IOException
+    {
+        Path temp = embulk.createTempFile("txt");
+        Files.delete(temp);
+        final String cols = columnList.stream().collect(Collectors.joining(","));
+        execute(String.format("\\COPY (SELECT %s FROM %s) TO '%s' With CSV DELIMITER ',';", cols,  tableName, temp.toString().replace("\\", "\\\\")));
+        return readSortedFile(temp);
+    }
+
+}
